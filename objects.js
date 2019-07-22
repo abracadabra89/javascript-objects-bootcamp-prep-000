98eca68de2da62778eb3d2a6589d03039e587c28
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-22 21:58:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,11 @@ var playlist = {}
 playlist[artistName] = songTitle
 
 playlist 
+
+var playlist = {} 
+playlist.Beyonce = 'fear' 
+playlist['BTS'] = 'fire' 
+
   
  function updatePlaylist(playlist, artist, song) { 
   playlist[artist]= song 
