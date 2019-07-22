be58f954d9a9f6a79012caaf51e1c67338f300fe
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-22 21:56:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,12 +1,15 @@
 let playlist = {
-  artistName: 'songTitle'
+  artist: 'song'
   };
+  
  function updatePlaylist(playlist, artist, song) { 
   playlist[artist]= song 
   return playlist
 }
  
- function removeFromPlaylist(playlist, artistName){
-   delete playlist.artistName;
-   return playlist;
- }
+ function removeFromPlaylist(playlist, artist) { 
+  delete playlist[artist]
+  return playlist 
+  
+  
+}
